8d94046bf3db85eae2b3823d7c78298d93120cf9
seantmyers4-source <seantmyers4@gmail.com>
2026-08-31 19:23:06 -0700

    INT-FOS: finish Garmin mapping lint cleanup

--- packages/connectors/garmin_mapping.py
+++ packages/connectors/garmin_mapping.py
@@ -18,10 +18,20 @@ MAPPING_VERSION: Final = "garmin.completed_activity.mapping.v1"
 
 COMPLETED_ACTIVITY_MAPPING: Final[tuple[GarminFieldMapping, ...]] = (
     GarminFieldMapping(
-        "activityId", "provider activity identity", None, None, "external_activity_id", MAPPING_VERSION
+        "activityId",
+        "provider activity identity",
+        None,
+        None,
+        "external_activity_id",
+        MAPPING_VERSION,
     ),
     GarminFieldMapping(
-        "activityType", "provider activity type", None, None, "activity_type_candidate", MAPPING_VERSION
+        "activityType",
+        "provider activity type",
+        None,
+        None,
+        "activity_type_candidate",
+        MAPPING_VERSION,
     ),
     GarminFieldMapping(
         "startTimeInSeconds",
