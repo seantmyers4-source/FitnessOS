aa90e272097fbf9174c0c18fd701e2c9abec6fef
seantmyers4-source <seantmyers4@gmail.com>
2026-09-06 17:34:46 -0700

    Use default IAM budget notification routing

--- infra/terraform/nonprod/main.tf
+++ infra/terraform/nonprod/main.tf
@@ -112,9 +112,5 @@ resource "google_billing_budget" "nonprod" {
     }
   }
 
-  all_updates_rule {
-    disable_default_iam_recipients = false
-  }
-
   depends_on = [google_project_service.required]
 }
